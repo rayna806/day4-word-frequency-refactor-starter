516360a54ec00b31957108a9ba906f2da8b10f32
rayna806 <LUORA2@OOCL.COM>
2025-09-04 16:12:05 +0800

    Refactor:countFrequencises clean up

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -51,14 +51,14 @@ public class WordFrequencyGame {
         }
         //get the map for the next step of sizing the same word
         Map<String, List<String>> map = new HashMap<>();
-        for (String input1 : inputList) {
+        for (String input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input1)) {
+            if (!map.containsKey(input)) {
                 ArrayList arr = new ArrayList<>();
-                arr.add(input1);
-                map.put(input1, arr);
+                arr.add(input);
+                map.put(input, arr);
             } else {
-                map.get(input1).add(input1);
+                map.get(input).add(input);
             }
         }
         return map;
